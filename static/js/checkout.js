e172c2946051d0a5c1b0b02ca819865449090eb1
Sneha Neupane <snehaneup@gmail.com>
2024-03-06 12:15:34 +0545

    First Commit

---
+++ static/js/checkout.js
@@ -0,0 +1,82 @@
+$(document).ready(function() {
+
+  $('.payWithRazorpay').click(function (e) { 
+    e.preventDefault();
+
+    var fname = $("[name='fname']").val();
+    var lname = $("[name='lname']").val();
+    var email = $("[name='email']").val();
+    var phone = $("[name='phone']").val();
+    var address = $("[name='address']").val();
+    var city = $("[name='city']").val();
+    var province = $("[name='province']").val();
+    var country = $("[name='country']").val();
+    var pincode = $("[name='pincode']").val();
+    var token = $("[name='csrfmiddlewaretoken']").val();
+
+    
+    if (fname == "" || lname == "" || email == "" || phone == "" || address == "" || city == "" || province == "" || country == "" || pincode == "")
+    {
+      swal("Alert!","All fields are mandatory","error");
+      return false;
+    }
+    else
+    {
+      $.ajax({
+        method: "GET",
+        url: "/proceed-to-pay",
+        success: function(response) {
+          var options = {
+            "key": "YOUR_KEY_ID", // Enter the Key ID generated from the Dashboard
+            "amount": 1*100,//response.total_price * 100, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+            "currency": "NPR",
+            "name": "Sneha Neupane", //your business name
+            "description": "Thank you for buying from us",
+            "image": "https://example.com/your_logo",
+            //"order_id": "order_9A33XWu170gUtm", //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+            "handler": function (responseb){
+              alert(responseb.razorpay_payment_id);
+              data = {
+                "fname": fname,
+                "lname": lname,
+                "email": email,
+                "phone": phone,
+                "address": address,
+                "city": city,
+                "province": province,
+                "country": country,
+                "pincode": pincode,
+                "payment_mode": "Paid by Razorpay",
+                "payment_id": responseb.razorpay_payment_id,
+                csrfmiddlewaretoken: token,
+              }
+              $.ajax({
+                method: "POST",
+                url: "/place-order",
+                data: data,
+                success: function(responsec) {
+                  swal("Congratulations!", responsec.status, "success").then((value) => {
+                    window.location.href = "/my-orders";
+                  });
+                },
+              });
+          
+            },
+            "prefill": {
+              "name": fname + " " + lname,
+              "email": email,
+              "contact": phone,
+            },
+            "theme": {
+              "color": "#3399cc",
+            }
+          };
+          var rzp1 = new Razorpay(options);
+          rzp1.open();
+        }
+      });
+    }
+  });    
+});
+
+ 
